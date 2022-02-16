12a576884c777ed484034a22ac25944ab0cbce61
RepoLink <92987939+RepoLink@users.noreply.github.com>
2022-02-16 16:50:24 +0630

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -59,8 +59,8 @@ def stats(update, context):
 
 def start(update, context):
     buttons = ButtonMaker()
-    buttons.buildbutton("Repo", "https://www.github.com/anasty17/mirror-leech-telegram-bot")
-    buttons.buildbutton("Report Group", "https://t.me/+PRRzqHd31XY3ZWZk")
+    buttons.buildbutton("Repo", "https://github.com/RepoLink/Mirror-Leech")
+    buttons.buildbutton("Contact", "https://t.me/Anonymous_yz")
     reply_markup = InlineKeyboardMarkup(buttons.build_menu(2))
     if CustomFilters.authorized_user(update) or CustomFilters.authorized_chat(update):
         start_string = f'''
